79a279001bee4084ab3442a2a1841d64c31fd074
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-08-12 11:21:38 -0300

    Add Iterables.sort

---
+++ src/main/java/br/eti/clairton/instancebypriority/Iterables.java
@@ -0,0 +1,18 @@
+package br.eti.clairton.instancebypriority;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Iterables {
+
+	public static <T> Iterable<T> sort(final Iterable<T> iterable, final Comparator<T> comparator) {
+		final List<T> list = new ArrayList<T>();
+		for (final T t : iterable ) {
+			list.add(t);
+		}
+		Collections.sort(list, comparator);
+		return list;
+	}
+}
---
+++ src/test/java/br/eti/clairton/instancebypriority/IterablesTest.java
@@ -0,0 +1,24 @@
+package br.eti.clairton.instancebypriority;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class IterablesTest {
+
+	@Test
+	public void test() {
+		final List<Interface> collection = Arrays.asList(new Class1(), new Class4(), new Class3(), new Class2());
+		final Iterable<Interface> iteratable = Iterables.sort(collection, new Comparator<Interface>());
+		Integer count = 1;
+		for (final Interface i : iteratable) {
+			final String name = i.getClass().getName();
+			assertTrue(name.contains("Class" + count));
+			count++;
+		}
+	}
+
+}
